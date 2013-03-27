a9277d57cd936bea6faea8785976198109cb5c42
David Goodlad <david@goodlad.net>
2013-03-27 20:26:38 +1100

    Show a vertical line on mouseover

--- js/main.js
+++ js/main.js
@@ -15,6 +15,8 @@
   function xVal(point) { return new Date(1000 * point[1]) }
   function yVal(point) { return point[0] }
 
+  var bisectDate = d3.bisector(function(d) { return xVal(d); }).left;
+
   function renderGraph(el, json) {
     var colors = d3.scale.category10();
 
@@ -86,6 +88,43 @@
       .classed("y-axis", true)
       .attr("transform", "translate(" + padding + ", 0)")
       .call(yAxis);
+
+    var focus = svg.append("g")
+      .attr("class", "focus")
+      .style("display", "none");
+
+    focus.append("line")
+      .attr("x1", 0)
+      .attr("x2", 0)
+      .attr("y1", 0)
+      .attr("y2", h - padding);
+
+    svg.append("rect")
+      .attr("class", "overlay")
+      .attr("x", padding)
+      .attr("y", 0)
+      .attr("width", w - 2 * padding)
+      .attr("height", h - padding)
+      .on("mouseover", function() { focus.style("display", null); })
+      .on("mouseout",  function() { focus.style("display", "none"); })
+      .on("mousemove", function() {
+        /* TODO validate the assumption that the first series in the json will
+         * have x values for the whole shebang */
+        var x0 = xScale.invert(d3.mouse(this)[0]),
+            i = bisectDate(json[0].datapoints, x0, 1),
+            d0 = json[0].datapoints[i - 1],
+            d1 = json[0].datapoints[i];
+        if(d0 && d1) {
+          var d = x0 - xVal(d0) > xVal(d1) - x0 ? d1 : d0;
+        } else if(d0) {
+          var d = d0;
+        } else if(d1) {
+          var d = d1;
+        } else {
+          console.log("Failed to get datapoint for highlight");
+        }
+        focus.attr("transform", "translate(" + xScale(xVal(d)) + ",0)");
+      });
   }
 
   getGraphSources().forEach(function(source) {
